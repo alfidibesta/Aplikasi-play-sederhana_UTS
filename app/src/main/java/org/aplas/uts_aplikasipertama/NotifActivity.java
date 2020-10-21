cc3b4ca2960cc5809cf920666948d52be6f584c8
Besta Alfidi <43779424+alfidibesta@users.noreply.github.com>
2020-10-21 09:12:54 +0700

    bismillah

---
+++ app/src/main/java/org/aplas/uts_aplikasipertama/NotifActivity.java
@@ -0,0 +1,14 @@
+package org.aplas.uts_aplikasipertama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotifActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notif);
+    }
+}
